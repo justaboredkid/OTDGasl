55f117b77a01cf01a30d9f7347db94233716f52a
Henry <29010153+justaboredkid@users.noreply.github.com>
2019-05-27 13:21:47 -0700

    initial commit

---
+++ ASLparser.go
@@ -0,0 +1,141 @@
+/*
+Program for parsing ASL using OTDG.
+
+OTDG IS NOT ONLY DESIGNED FOR ASL. OTDG = Open Tactile Data Glove
+
+Copyright (C) 2019  Henry Lo
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"time"
+
+	"github.com/justaboredkid/OTDGasl/asllibs"
+
+	"github.com/warthog618/gpio"
+)
+
+var debug *bool
+var glove asllibs.Hand
+var dict []asllibs.ASLdict // slice of ASLdict, not pointer
+
+// Checks and reading GPIO
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func pinRead(pin *gpio.Pin) bool {
+	pin.PullUp()
+	pin.Input()
+	return bool(pin.Read())
+}
+
+func init() {
+	err := gpio.Open()
+	check(err)
+
+	debug = flag.Bool("debug", false, "Enables Debug Logging")
+	flag.Parse()
+
+	// Go thorugh all files and folders in "data/"
+	err = filepath.Walk("data/",
+		func(path string, _ os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			// Check if path is folder
+			fi, err := os.Stat(path)
+			check(err)
+			if fi.IsDir() {
+				return nil
+			}
+
+			f, err := ioutil.ReadFile(path)
+			check(err)
+			fmt.Printf("%v loaded\n", path[5:])
+			json.Unmarshal(f, &dict)
+			return nil
+		})
+	check(err)
+}
+
+// Main recognition
+func main() {
+
+	if *debug {
+		for i := range dict {
+			fmt.Printf("ID: %v loaded\n", dict[i].ID)
+		}
+	}
+
+	fmt.Println("ASL Parsing [STARTED]")
+
+	// Graceful shutdown handling
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt)
+
+	go func() {
+		select {
+		case sig := <-c:
+			gpio.Close()
+			fmt.Printf("Got %s signal. Exiting...\n", sig)
+			os.Exit(1)
+		}
+	}()
+
+	// NOTE TO SELF: use location for signs pointing downwards (ie Q)
+	for true {
+		glove = asllibs.Hand{
+			Pinky:     pinRead(gpio.NewPin(2)),
+			Ring:      pinRead(gpio.NewPin(3)),
+			Middle:    pinRead(gpio.NewPin(4)),
+			Index:     pinRead(gpio.NewPin(5)),
+			Thumb:     pinRead(gpio.NewPin(6)),
+			PalmLeft:  pinRead(gpio.NewPin(7)),
+			PalmRight: pinRead(gpio.NewPin(8)),
+			BackThumb: pinRead(gpio.NewPin(9)),
+			BackRing:  pinRead(gpio.NewPin(10)),
+			BetwIM:    pinRead(gpio.NewPin(11)),
+			BetwMR:    pinRead(gpio.NewPin(12)),
+			BetwRP:    pinRead(gpio.NewPin(13)),
+			Angle:     -1,
+			Motion:    "",
+		}
+		time.Sleep(200 * time.Millisecond)
+		for i := range dict {
+			if glove == dict[i].Hand {
+				fmt.Printf("%v [MATCH]\n", dict[i].ID)
+			} else {
+				if *debug {
+					fmt.Printf("%v [NOT MATCHED]\n", dict[i].ID)
+				}
+			}
+		}
+	}
+
+}
---
+++ asllibs/structs.go
@@ -0,0 +1,54 @@
+// Package asllibs is a internal library for parsing ASL using the OTDG
+
+/*
+Copyright (C) 2019  Henry Lo
+
+This file is part of OTDGasl.
+
+    OTDGasl is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OTDGasl is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OTDGasl.  If not, see <https://www.gnu.org/licenses/>.
+*/
+package asllibs
+
+/*
+Define hand data
+Palm button orientation is FACING viewer
+Left is towards the pinky and so on
+
+Between finger is defined by betw, then the initial of the fingers
+*/
+type Hand struct {
+	Pinky     bool   `json:"pinky"`
+	Ring      bool   `json:"ring"`
+	Middle    bool   `json:"middle"`
+	Index     bool   `json:"index"`
+	Thumb     bool   `json:"thumb"`
+	PalmLeft  bool   `json:"palmLeft"`
+	PalmRight bool   `json:"palmRight"`
+	BackThumb bool   `json:"backThumb"`
+	BackRing  bool   `json:"backRing"`
+	BetwIM    bool   `json:"betwIM"`
+	BetwMR    bool   `json:"betwMR"`
+	BetwRP    bool   `json:"betwRP"`
+	Angle     int    `json:"angle"`
+	Motion    string `json:"motion"`
+	Dom       bool   `json:"dom"`
+}
+
+// ASLdict is a struct for parsing the ASL JSON files
+type ASLdict struct {
+	// BUG(justaboredkid): add expression
+	ID       string `json:"id"`
+	Hand     Hand   `json:"hand"`
+	Location string `json:"location"`
+}
